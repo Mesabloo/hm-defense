4450e2cf6482a1edeadcfbfb2cf2776fe233edfa
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-12-02 19:37:52 +0100

    use the compact syntax for dependency import

--- core/src/kotlin/fr/mesabloo/heavymachdefense/MainGame.kt
+++ core/src/kotlin/fr/mesabloo/heavymachdefense/MainGame.kt
@@ -4,40 +4,32 @@ import com.badlogic.gdx.Application
 import com.badlogic.gdx.Gdx
 import fr.mesabloo.heavymachdefense.log.ColoredLogger
 import ktx.app.KtxApplicationAdapter
-import ktx.log.debug
 
 class MainGame : KtxApplicationAdapter {
     override fun create() {
         Gdx.app.applicationLogger = ColoredLogger()
         Gdx.app.logLevel = if (DEBUG) Application.LOG_DEBUG else Application.LOG_INFO
 
-
-
-        if (DEBUG) {
-            debug(javaClass.canonicalName) { "Hello" }
-        }
-
-        TODO("Not yet implemented")
     }
 
     override fun resize(width: Int, height: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun render() {
-        TODO("Not yet implemented")
+
     }
 
     override fun pause() {
-        TODO("Not yet implemented")
+
     }
 
     override fun resume() {
-        TODO("Not yet implemented")
+
     }
 
     override fun dispose() {
-        TODO("Not yet implemented")
+
     }
 
 }
