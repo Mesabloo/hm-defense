c233387dbeed8b1a99482365488d1ee1a6f27c88
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2022-05-14 18:14:31 +0200

    reorganize sound assets

---
+++ core/assets/sfx/MAPPING.md
@@ -0,0 +1,99 @@
+# Sound mappings
+
+## Machine sounds
+
+- `HMG01.wav` → `machine/hmg/01.wav`
+- `HMG02.wav` → `machine/hmg/02.wav`
+- `HMG03.wav` → `machine/hmg/03.wav`
+- `HMG04.wav` → `machine/hmg/04.wav`
+- `HMG05.wav` → `machine/hmg/05.wav`
+- `HMG06.wav` → `machine/hmg/06.wav`
+- `HMG07.wav` → `machine/hmg/07.wav`
+- `HMG08.wav` → `machine/hmg/08.wav`
+- `HMG09.wav` → `machine/hmg/09.wav`
+- `HMG10.wav` → `machine/hmg/10.wav`
+- `Ion01.wav` → `machine/ion/01.wav`
+- `Ion02.wav` → `machine/ion/02.wav`
+- `Ion03.wav` → `machine/ion/03.wav`
+- `MachineGun01.wav` → `machine/rifle/01.wav`
+- `MachineGun02.wav` → `machine/rifle/02.wav`
+- `MachineGun03.wav` → `machine/rifle/03.wav`
+- `MachineGun04.wav` → `machine/rifle/04.wav`
+- `MachineGun05.wav` → `machine/rifle/05.wav`
+- `MachineGun06.wav` → `machine/rifle/06.wav`
+- `MachineGun07.wav` → `machine/rifle/07.wav`
+- `MachineGun08.wav` → `machine/rifle/08.wav`
+- `MachineGun09.wav` → `machine/rifle/09.wav`
+- `MachineGun10.wav` → `machine/rifle/10.wav`
+- `Missile01.wav` → `machine/missile/01.wav`
+- `Missile02.wav` → `machine/missile/02.wav`
+- `Missile03.wav` → `machine/missile/03.wav`
+- `Missile04.wav` → `machine/missile/04.wav`
+- `Missile05.wav` → `machine/missile/05.wav`
+- `Missile06.wav` → `machine/missile/06.wav`
+- `Missile07.wav` → `machine/missile/07.wav`
+- `Missile08.wav` → `machine/missile/08.wav`
+- `Missile09.wav` → `machine/missile/09.wav`
+- `Missile10.wav` → `machine/missile/10.wav`
+- `Plasma01.wav` → `machine/plasma/01.wav`
+- `Plasma02.wav` → `machine/plasma/02.wav`
+- `Plasma03.wav` → `machine/plasma/03.wav`
+- `Plasma04.wav` → `machine/plasma/04.wav`
+- `Plasma05.wav` → `machine/plasma/05.wav`
+- `Shotgun01.wav` → `machine/shotgun/01.wav`
+- `Shotgun02.wav` → `machine/shotgun/02.wav`
+- `Shotgun03.wav` → `machine/shotgun/03.wav`
+
+## UI
+
+- `UI_BGM.mp3` → `ui/background-music.mp3`
+- `UI_Btn_Cancel.wav` → `ui/button-cancel.wav`
+- `UI_Btn_OK.wav` → `ui/button-ok.wav`
+- `UI_Click.wav` → `ui/click.wav`
+- `UIType_BuildMach.wav` → `ui/button-build-mach.wav`
+- `UIType_Formation.wav` → `ui/button-formation.wav`
+- `UIType_SpAttack.wav` → `ui/button-special.wav`
+
+## Effects
+
+- `SFX_AirStrike.wav` → `effects/airstrike.wav`
+- `SFX_ExplGround.wav` → `effects/ground-explosion.wav`
+- `SFX_ExplMach.wav` → `effects/mach-explosion.wav`
+- `SFX_GroundHit.wav` → `effects/ground-hit.wav`
+- `SFX_HitBody01.wav` → `effects/body-hit-01.wav`
+- `SFX_HitBody02.wav` → `effects/body-hit-02.wav`
+- `SFX_HitBody03.wav` → `effects/body-hit-03.wav`
+- `SFX_HitBody04.wav` → `effects/body-hit-04.wav`
+- `SFX_OnMessage.wav` → `effects/on-message.wav`
+- `SFX_QuickSlot.wav` → `effects/quick-slot.wav`
+
+## Game sounds
+
+- `BuildMachComplete.wav` → `game/build-complete.wav`
+- `BuildMach.wav` → `game/build-started.wav`
+- `BuySpAttack.wav` → `game/buy-special.wav`
+- `DmgPlasma.wav` → `game/plasma-damage.wav`
+- `Game_GetItem.wav` → `game/get-item.wav`
+- `Game_LowHP.wav` → `game/low-hp.wav`
+- `Install.wav` → `game/install.wav`
+- `Uninstall.wav` → `game/uninstall.wav`
+- `UpgradeBase.wav` → `game/upgrade-base.wav`
+- `UpgradeMach.wav` → `game/upgrade-mach.wav`
+- `UpgradeSpAttack.wav` → `game/upgrade-special.wav`
+
+## Weapons
+
+- `Wpn_Default.wav` → `weapon/default.wav`
+- `Wpn_EnCanon.wav` → `weapon/cannon.wav`
+- `Wpn_EnGun.wav` → `weapon/gun.wav`
+- `Wpn_EnMissile.wav` → `weapon/missile.wav`
+
+## Other
+
+- `GamePlay.mp3` → `misc/gameplay.mp3`
+- `Intro.mp3` → `misc/intro.mp3`
+- `Loading_Close.wav` → `misc/loading-close.wav`
+- `Loading_Open.wav` → `misc/loading-open.wav`
+- `Title_Bang.wav` → `misc/title-bang.wav`
+- `Title_TapToStart.wav` → `misc/title-tap-to-start.wav`
+- `Title_TitleIn.wav` → `misc/title-in.wav`
---
+++ core/assets/sfx/organize_sounds.sh
@@ -0,0 +1,246 @@
+#!/usr/bin/env bash
+
+(
+
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG01.wav' 'machine/hmg/01.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '0' 'HMG01.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG02.wav' 'machine/hmg/02.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '1' 'HMG02.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG03.wav' 'machine/hmg/03.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '2' 'HMG03.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG04.wav' 'machine/hmg/04.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '3' 'HMG04.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG05.wav' 'machine/hmg/05.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '5' 'HMG05.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG06.wav' 'machine/hmg/06.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '6' 'HMG06.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG07.wav' 'machine/hmg/07.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '7' 'HMG07.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG08.wav' 'machine/hmg/08.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '8' 'HMG08.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG09.wav' 'machine/hmg/09.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '10' 'HMG09.wav'
+  mkdir -p './machine/hmg' &>/dev/null || true
+  cp 'HMG10.wav' 'machine/hmg/10.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '11' 'HMG10.wav'
+  mkdir -p './machine/ion' &>/dev/null || true
+  cp 'Ion01.wav' 'machine/ion/01.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '12' 'Ion01.wav'
+  mkdir -p './machine/ion' &>/dev/null || true
+  cp 'Ion02.wav' 'machine/ion/02.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '13' 'Ion02.wav'
+  mkdir -p './machine/ion' &>/dev/null || true
+  cp 'Ion03.wav' 'machine/ion/03.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '15' 'Ion03.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun01.wav' 'machine/rifle/01.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '16' 'MachineGun01.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun02.wav' 'machine/rifle/02.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '17' 'MachineGun02.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun03.wav' 'machine/rifle/03.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '18' 'MachineGun03.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun04.wav' 'machine/rifle/04.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '20' 'MachineGun04.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun05.wav' 'machine/rifle/05.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '21' 'MachineGun05.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun06.wav' 'machine/rifle/06.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '22' 'MachineGun06.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun07.wav' 'machine/rifle/07.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '23' 'MachineGun07.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun08.wav' 'machine/rifle/08.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '25' 'MachineGun08.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun09.wav' 'machine/rifle/09.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '26' 'MachineGun09.wav'
+  mkdir -p './machine/rifle' &>/dev/null || true
+  cp 'MachineGun10.wav' 'machine/rifle/10.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '27' 'MachineGun10.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile01.wav' 'machine/missile/01.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '28' 'Missile01.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile02.wav' 'machine/missile/02.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '30' 'Missile02.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile03.wav' 'machine/missile/03.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '31' 'Missile03.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile04.wav' 'machine/missile/04.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '32' 'Missile04.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile05.wav' 'machine/missile/05.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '33' 'Missile05.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile06.wav' 'machine/missile/06.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '35' 'Missile06.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile07.wav' 'machine/missile/07.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '36' 'Missile07.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile08.wav' 'machine/missile/08.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '37' 'Missile08.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile09.wav' 'machine/missile/09.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '38' 'Missile09.wav'
+  mkdir -p './machine/missile' &>/dev/null || true
+  cp 'Missile10.wav' 'machine/missile/10.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '40' 'Missile10.wav'
+  mkdir -p './machine/plasma' &>/dev/null || true
+  cp 'Plasma01.wav' 'machine/plasma/01.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '41' 'Plasma01.wav'
+  mkdir -p './machine/plasma' &>/dev/null || true
+  cp 'Plasma02.wav' 'machine/plasma/02.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '42' 'Plasma02.wav'
+  mkdir -p './machine/plasma' &>/dev/null || true
+  cp 'Plasma03.wav' 'machine/plasma/03.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '43' 'Plasma03.wav'
+  mkdir -p './machine/plasma' &>/dev/null || true
+  cp 'Plasma04.wav' 'machine/plasma/04.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '45' 'Plasma04.wav'
+  mkdir -p './machine/plasma' &>/dev/null || true
+  cp 'Plasma05.wav' 'machine/plasma/05.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '46' 'Plasma05.wav'
+  mkdir -p './machine/shotgun' &>/dev/null || true
+  cp 'Shotgun01.wav' 'machine/shotgun/01.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '47' 'Shotgun01.wav'
+  mkdir -p './machine/shotgun' &>/dev/null || true
+  cp 'Shotgun02.wav' 'machine/shotgun/02.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '48' 'Shotgun02.wav'
+  mkdir -p './machine/shotgun' &>/dev/null || true
+  cp 'Shotgun03.wav' 'machine/shotgun/03.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '50' 'Shotgun03.wav'
+  mkdir -p './ui' &>/dev/null || true
+  cp 'UI_BGM.mp3' 'ui/background-music.mp3' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '51' 'UI_BGM.mp3'
+  mkdir -p './ui' &>/dev/null || true
+  cp 'UI_Btn_Cancel.wav' 'ui/button-cancel.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '52' 'UI_Btn_Cancel.wav'
+  mkdir -p './ui' &>/dev/null || true
+  cp 'UI_Btn_OK.wav' 'ui/button-ok.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '53' 'UI_Btn_OK.wav'
+  mkdir -p './ui' &>/dev/null || true
+  cp 'UI_Click.wav' 'ui/click.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '55' 'UI_Click.wav'
+  mkdir -p './ui' &>/dev/null || true
+  cp 'UIType_BuildMach.wav' 'ui/button-build-mach.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '56' 'UIType_BuildMach.wav'
+  mkdir -p './ui' &>/dev/null || true
+  cp 'UIType_Formation.wav' 'ui/button-formation.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '57' 'UIType_Formation.wav'
+  mkdir -p './ui' &>/dev/null || true
+  cp 'UIType_SpAttack.wav' 'ui/button-special.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '58' 'UIType_SpAttack.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_AirStrike.wav' 'effects/airstrike.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '60' 'SFX_AirStrike.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_ExplGround.wav' 'effects/ground-explosion.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '61' 'SFX_ExplGround.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_ExplMach.wav' 'effects/mach-explosion.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '62' 'SFX_ExplMach.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_GroundHit.wav' 'effects/ground-hit.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '63' 'SFX_GroundHit.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_HitBody01.wav' 'effects/body-hit-01.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '65' 'SFX_HitBody01.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_HitBody02.wav' 'effects/body-hit-02.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '66' 'SFX_HitBody02.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_HitBody03.wav' 'effects/body-hit-03.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '67' 'SFX_HitBody03.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_HitBody04.wav' 'effects/body-hit-04.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '68' 'SFX_HitBody04.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_OnMessage.wav' 'effects/on-message.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '70' 'SFX_OnMessage.wav'
+  mkdir -p './effects' &>/dev/null || true
+  cp 'SFX_QuickSlot.wav' 'effects/quick-slot.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '71' 'SFX_QuickSlot.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'BuildMachComplete.wav' 'game/build-complete.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '72' 'BuildMachComplete.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'BuildMach.wav' 'game/build-started.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '73' 'BuildMach.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'BuySpAttack.wav' 'game/buy-special.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '75' 'BuySpAttack.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'DmgPlasma.wav' 'game/plasma-damage.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '76' 'DmgPlasma.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'Game_GetItem.wav' 'game/get-item.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '77' 'Game_GetItem.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'Game_LowHP.wav' 'game/low-hp.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '78' 'Game_LowHP.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'Install.wav' 'game/install.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '80' 'Install.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'Uninstall.wav' 'game/uninstall.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '81' 'Uninstall.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'UpgradeBase.wav' 'game/upgrade-base.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '82' 'UpgradeBase.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'UpgradeMach.wav' 'game/upgrade-mach.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '83' 'UpgradeMach.wav'
+  mkdir -p './game' &>/dev/null || true
+  cp 'UpgradeSpAttack.wav' 'game/upgrade-special.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '85' 'UpgradeSpAttack.wav'
+  mkdir -p './weapon' &>/dev/null || true
+  cp 'Wpn_Default.wav' 'weapon/default.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '86' 'Wpn_Default.wav'
+  mkdir -p './weapon' &>/dev/null || true
+  cp 'Wpn_EnCanon.wav' 'weapon/cannon.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '87' 'Wpn_EnCanon.wav'
+  mkdir -p './weapon' &>/dev/null || true
+  cp 'Wpn_EnGun.wav' 'weapon/gun.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '88' 'Wpn_EnGun.wav'
+  mkdir -p './weapon' &>/dev/null || true
+  cp 'Wpn_EnMissile.wav' 'weapon/missile.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '90' 'Wpn_EnMissile.wav'
+  mkdir -p './misc' &>/dev/null || true
+  cp 'GamePlay.mp3' 'misc/gameplay.mp3' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '91' 'GamePlay.mp3'
+  mkdir -p './misc' &>/dev/null || true
+  cp 'Intro.mp3' 'misc/intro.mp3' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '92' 'Intro.mp3'
+  mkdir -p './misc' &>/dev/null || true
+  cp 'Loading_Close.wav' 'misc/loading-close.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '93' 'Loading_Close.wav'
+  mkdir -p './misc' &>/dev/null || true
+  cp 'Loading_Open.wav' 'misc/loading-open.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '95' 'Loading_Open.wav'
+  mkdir -p './misc' &>/dev/null || true
+  cp 'Title_Bang.wav' 'misc/title-bang.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '96' 'Title_Bang.wav'
+  mkdir -p './misc' &>/dev/null || true
+  cp 'Title_TapToStart.wav' 'misc/title-tap-to-start.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '97' 'Title_TapToStart.wav'
+  mkdir -p './misc' &>/dev/null || true
+  cp 'Title_TitleIn.wav' 'misc/title-in.wav' &>/dev/null
+  printf "XXX\n%d\n\nProcessing file %s\nXXX\n" '98' 'Title_TitleIn.wav'
+
+) | dialog --title 'Moving assets to the correct location' --gauge '\n' 7 80 0
