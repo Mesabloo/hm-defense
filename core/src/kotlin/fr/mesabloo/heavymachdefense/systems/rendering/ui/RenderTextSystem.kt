4acc24799051e7d83d1f900cb20a9d60784d384f
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2022-05-19 12:31:05 +0200

    correctly size the UI viewport

--- core/src/kotlin/fr/mesabloo/heavymachdefense/systems/rendering/ui/RenderTextSystem.kt
+++ core/src/kotlin/fr/mesabloo/heavymachdefense/systems/rendering/ui/RenderTextSystem.kt
@@ -2,6 +2,7 @@ package fr.mesabloo.heavymachdefense.fr.mesabloo.heavymachdefense.systems.render
 
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import fr.mesabloo.heavymachdefense.fr.mesabloo.heavymachdefense.components.PositionComponent
 import fr.mesabloo.heavymachdefense.fr.mesabloo.heavymachdefense.components.ui.TextComponent
@@ -10,13 +11,23 @@ import ktx.ashley.get
 
 class RenderTextSystem(private val batch: SpriteBatch) :
     IteratingSystem(allOf(TextComponent::class, PositionComponent::class).get()) {
+
+    private val glyphLayout = GlyphLayout()
+
     override fun processEntity(entity: Entity?, deltaTime: Float) {
         if (entity != null) {
             val textComponent = entity[TextComponent.mapper]!!
             val positionComponent = entity[PositionComponent.mapper]!!
 
+            glyphLayout.setText(textComponent.font, textComponent.message)
+
             this.batch.begin()
-            textComponent.font.draw(this.batch, textComponent.message, positionComponent.x, positionComponent.y)
+            textComponent.font.draw(
+                this.batch,
+                textComponent.message,
+                positionComponent.x - glyphLayout.width / 2,
+                positionComponent.y - glyphLayout.height / 2
+            )
             this.batch.end()
         }
     }
--- core/src/kotlin/fr/mesabloo/heavymachdefense/world/UIWorld.kt
+++ core/src/kotlin/fr/mesabloo/heavymachdefense/world/UIWorld.kt
@@ -10,15 +10,15 @@ const val UI_WIDTH = 768f // = 768f
 const val UI_HEIGHT = 1024f // the game screen is actually twice as high, but we only show half of it
 
 class UIWorld : Disposable {
-    val camera: OrthographicCamera = OrthographicCamera(WORLD_WIDTH, WORLD_HEIGHT)
-    private val viewport: ExtendViewport = ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, this.camera)
+    val camera: OrthographicCamera = OrthographicCamera(UI_WIDTH, UI_HEIGHT)
+    private val viewport: ExtendViewport = ExtendViewport(UI_WIDTH, UI_HEIGHT, this.camera)
 
     val batch = SpriteBatch()
 
     val engine = PooledEngine()
 
     init {
-        this.camera.position.set(WORLD_WIDTH / 2 + 128, WORLD_HEIGHT / 2, 0f)
+        this.camera.position.set(UI_WIDTH / 2, UI_HEIGHT / 2, 0f)
         this.camera.update()
     }
 
